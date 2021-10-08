a8da4d8715c37c795e8c7ba70348a63c591987fe
rds80 <rippleshah@msn.com>
2021-10-07 22:42:55 -0400

    Initial commit

---
+++ src/WeatherData.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherData implements  Subject {
+
+    private List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        this.observers = new ArrayList<Observer>();
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer: observers) {
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+}
